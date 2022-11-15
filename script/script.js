167a50a6c28e5e39a765593beed31e7e28c30254
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-14 23:32:04 -0300

    Três objetos criados (Fase - Modelagem)

---
+++ script/script.js
@@ -0,0 +1,35 @@
+// PRIMEIRA PARTE - MODELAGEM 
+
+
+// 1º Objeto 
+
+let objetoPessoa = {
+    nome: "Lucas",
+    anoDeNascimento: 1997,
+    cpf: 46154821853,
+    cidade: "São Paulo",
+    estado: "São Paulo",
+    logradouro: "Rua",
+    curso: "Programação"
+}
+
+// 2º Objeto
+
+let objetoEscola = {
+    nome: "Colégio Soer",
+    cnpj: 0707077000144,
+    areaDeAtuacao: "Educação",
+    cidade: "Rio de Janeiro",
+    estado: "Rio de Janeiro",
+    logradouro: "Avenida",
+    curso: "Ensino Médio"
+}
+
+// 3º Objeto
+
+let objetoCurso = {
+    nome: "Curso de Inglês",
+    duracaoEmAnos: 4,
+    turma: 51,
+    modulos: 9
+}
